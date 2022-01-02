14c70073e2824dd524d1c26f7e9cd108fa23fbf3
LEE HYO JEONG <hyojeong1913@naver.com>
2022-01-02 20:22:51 +0900

    변수 - SpringEL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -43,5 +49,61 @@ public class BasicController {
         model.addAttribute("data", "Hello <b>Spring!</b>");
 
         return "basic/text-unescape";
+    }
+
+    /**
+     * SpringEL 다양한 표현식 사용
+     *
+     * Object
+     * - user.username : user 의 username 을 프로퍼티 접근 => user.getUsername()
+     * - user['username'] : user 의 username 을 프로퍼티 접근 => user.getUsername()
+     * - user.getUsername() : user 의 getUsername() 을 직접 호출
+     *
+     * List
+     * - users[0].username : List 에서 첫 번째 회원을 찾고 username 프로퍼티 접근 => list.get(0).getUsername()
+     * - users[0]['username'] : List 에서 첫 번째 회원을 찾고 username 프로퍼티 접근 => list.get(0).getUsername()
+     * - users[0].getUsername() : List 에서 첫 번째 회원을 찾고 메서드 직접 호출
+     *
+     * Map
+     * - userMap['userA'].username : Map 에서 userA 를 찾고, username 프로퍼티 접근 => map.get("userA").getUsername()
+     * - userMap['userA']['username'] : Map 에서 userA 를 찾고, username 프로퍼티 접근 => map.get("userA").getUsername()
+     * - userMap['userA'].getUsername() : Map 에서 userA 를 찾고 메서드 직접 호출
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/variable")
+    public String variable(Model model) {
+
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 10);
+
+        List<User> list = new ArrayList<>();
+
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
