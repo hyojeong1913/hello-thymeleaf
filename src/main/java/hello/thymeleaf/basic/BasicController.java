f93876c42c42aa0dd82634eb69be55079aa446ab
LEE HYO JEONG <hyojeong1913@naver.com>
2022-01-05 06:20:22 +0900

    URL 링크

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -141,6 +141,21 @@ public class BasicController {
         return "basic/date";
     }
 
+    /**
+     * URL 링크
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/link")
+    public String link(Model model) {
+
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
     @Component("helloBean")
     static class HelloBean {
 
