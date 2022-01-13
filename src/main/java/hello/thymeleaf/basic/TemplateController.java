373c18d2062f0ea6d0f3dcd771c1e7517be156fd
LEE HYO JEONG <hyojeong1913@naver.com>
2022-01-14 00:05:40 +0900

    템플릿 조각

---
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,23 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    /**
+     * 템플릿 조각
+     *
+     * thymeleaf 는 템플릿 조각과 레이아웃 기능을 지원
+     *
+     * @return
+     */
+    @GetMapping("fragment")
+    public String template() {
+
+        return "template/fragment/fragmentMain";
+    }
+}
